88f9b5f58fa01135c207bccfa3666898732271d4
MukeshKaparapu <mukeshkaparapu@gmail.com>
2026-06-23 00:01:02 +0530

    First Commit

---
+++ springWebMVC1/src/main/java/com/mycompany/springWebMVC1/SpringWebMvc1Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.springWebMVC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvc1Application.class, args);
+	}
+
+}
---
+++ springWebMVC1/src/test/java/com/mycompany/springWebMVC1/SpringWebMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.springWebMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
