c289a108a6f6702a89c93ff79adb60612a0bf16b
MukeshKaparapu <mukeshkaparapu@gmail.com>
2026-06-23 09:06:19 +0530

    Created first webapp, controller annotation, requestmapping

---
+++ springWebMVC1/src/main/java/com/mycompany/springWebMVC1/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.mycompany.springWebMVC1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FirstController {
+	
+	@RequestMapping("/telusko")
+	public ModelAndView displayInfo() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", "This is my first webapp");
+		mav.setViewName("index");
+		
+		return mav;
+	}
+
+}
